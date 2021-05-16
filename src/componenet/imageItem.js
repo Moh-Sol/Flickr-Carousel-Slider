312aa0ec242f93d7c41975ce7281f3c558556b3d
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-05-16 11:47:35 +0200

    v2

--- src/componenet/imageItem.js
+++ src/componenet/imageItem.js
@@ -2,7 +2,20 @@ import React, { useState } from "react"
 import { motion } from 'framer-motion';
 
 
+
+
+
+
 function ImageItem(props) {
+
+    let imageSize = props.photoLink;
+
+    /* Denna funktion för att ändra query i api länken som ger oss storlekn på image */
+    function changeImgSize(img) {
+        return img.replace('_n', '_b');
+    }
+
+    changeImgSize(imageSize);
 
     const [showDescription, setShowDescription] = useState('none');   /* display ska vara 'none' som standard  */
 
@@ -11,7 +24,7 @@ function ImageItem(props) {
             // obs. detta kan jag fixa en vanlig css-kod också.
             onHoverStart={() => setShowDescription('block')} /* on hover - display : block  */
             onHoverEnd={() => setShowDescription('none')} /*  hover end  - display : none  */
-            onClick={() => window.location.href = `${props.photoLink} `  /* för att öpnna image vid klick */
+            onClick={() => window.location.href = `${changeImgSize(imageSize)} `  /* för att öpnna image på ett större storlek vid klick */
             }
 
         >
